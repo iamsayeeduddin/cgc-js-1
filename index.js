9317638f0e4e01728faba9b4db1961a8ffbba162
Sayeeduddin <iamsayeeduddin@outlook.com>
2025-02-11 19:13:31 +0530

    JS - 2

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-console.log("Hello World!");
-var age = "15";
-console.log(age);
-age = false;
-console.log(age);
+// console.log("Hello World!");
+// var age = "15";
+// console.log(age);
+// age = false;
+// console.log(age);
+var a;
+a = 10;
+a = 199;
+console.log(document.getElementsByTagName("body"));
+
+// Data Types
+// string
+// number = 10, -1, 1.98
+// boolean = true or false
+// array = []
+// objects = {}
+//function
---
+++ index1.js
@@ -0,0 +1,19 @@
+// String
+
+var name = "10%";
+// numbers
+var age = 17.5;
+
+// Operators
+
+// Arithmetic
+
+var a = 0;
+
+/// Assignment Operator
+
+// =
+a = a + 10;
+a += 10;
+var b = ++a;
+console.log(b);

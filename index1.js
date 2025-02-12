fdc4869a8cef5b3e9323b9af2b611dd7db06d842
Sayeeduddin <iamsayeeduddin@outlook.com>
2025-02-12 19:07:35 +0530

    JS 3

--- index1.js
+++ index1.js
@@ -15,5 +15,35 @@ var a = 0;
 // =
 a = a + 10;
 a += 10;
-var b = ++a;
-console.log(b);
+var b = a++;
+b++;
+// console.log(b)
+
+// Comparison Operatorva
+var c = false;
+var d = true;
+
+// console.log(d > c);
+// console.log(1 < 2);
+
+var n = "5";
+var txt = n + 5;
+console.log(txt);
+
+// AND OR NOT
+
+// Falsy = false, 0, undefined, "", null,
+// truthy anything besides falsy values = {}, [] " "
+
+// A | B  | R
+// T | T  | T && T = T
+// T | F  | T && F = F
+// F | T  | F && T = F
+// F | F  | F && F = F
+
+// A | B  | R
+// T | T  | T || T = T
+// T | F  | T || F = T
+// F | T  | F || T = T
+// F | F  | F || F = F
+console.log(!null);

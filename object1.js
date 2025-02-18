7f8f57aa179c69b5d3326e33d2b91866e192d0a6
Sayeeduddin <iamsayeeduddin@outlook.com>
2025-02-18 21:37:08 +0530

    Objects

---
+++ object1.js
@@ -0,0 +1,15 @@
+var m1 = {
+  brand: "Apple",
+  model: "Iphone 16",
+  color: "Silk Gold",
+  width: 5,
+  height: 7,
+};
+
+var age = 10;
+var age1 = age;
+age1 = 11;
+var m2 = m1;
+m2.color = "Red";
+// console.log(age, age1);
+console.log(m1, m2);
--- objects.js
+++ objects.js
@@ -1,11 +1,70 @@
-var student = {
-  name: "Syed Sayeeduddin",
-  mobile: "9874563210",
-  isRegistered: true,
-  "courses name": ["FSD", "Cybersecurity", "SOC"],
+// var student = {
+//   name: "Syed Sayeeduddin",
+//   mobile: "9874563210",
+//   isRegistered: true,
+//   "courses name": ["FSD", "Cybersecurity", "SOC"],
+//   call: function () {
+//     console.log("Calling Sayeed");
+//   },
+// };
+
+// console.log(student.call());
+
+var contact1 = {
+  name: {
+    first: "Syed",
+    middle: "Sayeed",
+    last: "Uddin",
+  },
+  number: [
+    {
+      value: 987456321,
+      isPrimary: false,
+      type: "Personal",
+    },
+    {
+      value: 9632587410,
+      isPrimary: true,
+      type: "Work",
+    },
+  ],
+  isActive: true,
   call: function () {
-    console.log("Calling Sayeed");
+    var numberToCall = this.number.find(function (val) {
+      // return val.isPrimary === true;
+      return val.isPrimary;
+    });
+    if (numberToCall !== undefined) {
+      console.log("Calling " + numberToCall.value);
+    } else {
+      console.log("Primary Number not found!");
+    }
   },
 };
 
-console.log(student.call());
+var selectedNumObj = contact1.number[1];
+// console.log(selectedNumObj.value);
+
+// function callPrimaryNumber() {
+//   var numberToCall = contact1.number.find(function (val) {
+//     // return val.isPrimary === true;
+//     return val.isPrimary;
+//   });
+//   if (numberToCall !== undefined) {
+//     console.log("Calling " + numberToCall.value);
+//   } else {
+//     console.log("Primary Number not found!");
+//   }
+// }
+
+// callPrimaryNumber();
+
+// console.log(contact1);
+// contact1.email = [
+//   { val: "iamsayeeduddin@gmail.com", isPrimary: true },
+//   { val: "test@gmail.com", isPrimary: false },
+// ];
+// contact1.name.last = "Shaik";
+// delete contact1.isActive;
+// console.log(contact1);
+contact1.call();

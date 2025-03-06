8db871911da0d87d30966de1dc547d628973f594
Sayeeduddin <iamsayeeduddin@outlook.com>
2025-03-06 08:33:51 +0530

    Strict Mode and Closures

---
+++ closures.js
@@ -0,0 +1,28 @@
+function greet() {
+  let name = "Sayeed";
+  function displayName() {
+    console.log(`Hi ${name}!`);
+  }
+  //   displayName();
+  return displayName;
+}
+
+// greet("Sayeed");
+// const g1 = greet("Sayeed");
+const g1 = greet()();
+// displayName();
+console.log(g1);
+// g1();
+
+function aA(a) {
+  return function bB(b) {
+    return function cC(c) {
+      return a + b + c;
+    };
+  };
+}
+
+console.log(aA(1)(2)(3));
+
+/// Factorial
+// 5 = 5*4*3*2*1
---
+++ modes.js
@@ -0,0 +1,24 @@
+"use strict";
+console.log("Hiiiii");
+
+let myVar;
+myVar = 0;
+console.log(myVar);
+
+function call(p1, p2) {
+  // function call(p1, p1) { // DUPLICATE PARAMS NOT ALLOWED
+  //   "use strict";
+  console.log(p1);
+  string = "Testing";
+  console.log(string);
+}
+
+call(1, 2);
+
+let obj = {
+  name: "Sayeed",
+};
+
+// delete obj;
+
+console.log(obj);
